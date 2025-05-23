926c6b49c85046df2249663c637f0d9a322fcb4d
mrezakhalafi <mrezakhalafi@yahoo.com>
2025-05-23 16:39:57 +0700

    Xendit Payment Method

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 import React, { useEffect } from "react";
+import { QRCodeSVG } from 'qrcode.react';
 
 export default function Admin() {
   const [amount, setAmount] = React.useState('');
   const [price, setPrice] = React.useState('');
+  const [qr, setQR] = React.useState('');
 
   const checkPay = async () => {
-    const response = await fetch(`http://localhost:5050/api/balance`);
+    const response = await fetch(`http://localhost:5000/api/balance`);
     const data = await response.json();
     setAmount(data.balance);
   };
 
   const checkPayment = async () => {
-    const response = await fetch(`http://localhost:5050/api/check-payment/${localStorage.getItem('id_payment')}`);
+    const response = await fetch(`http://localhost:5000/api/check-payment/${localStorage.getItem('id_payment')}`);
     const data = await response.json();
     console.log(data);
 
@@ -32,7 +34,7 @@ export default function Admin() {
   }, []);
 
   async function payWithOVO() {
-    const response = await fetch('http://localhost:5050/api/ovo-payment', {
+    const response = await fetch('http://localhost:5000/api/ovo-payment', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -43,8 +45,34 @@ export default function Admin() {
     });
 
     const data = await response.json();
-    if (response.ok) {
 
+    if (response.ok) {
+      // console.log(data);
+      localStorage.setItem('id_payment', data.id);
+
+      setTimeout(() => {
+        checkPayment();
+      }, 5000);
+      // window.location.href = data.url;
+    } else {
+      console.error('Payment error:', data.error);
+    }
+  }
+
+  async function payWithDana() {
+    const response = await fetch('http://localhost:5000/api/dana-payment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount: 50000,
+        externalID: 'invoice-12345',
+        phone: '081234567890',
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
       // console.log(data);
       localStorage.setItem('id_payment', data.id);
       window.location.href = data.url;
@@ -53,17 +81,111 @@ export default function Admin() {
     }
   }
 
+  async function payWithShopeePay() {
+    const response = await fetch('http://localhost:5000/api/shopeepay-payment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount: 50000,
+        externalID: 'invoice-12345',
+        phone: '081234567890',
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // console.log(data);
+      localStorage.setItem('id_payment', data.id);
+      window.location.href = data.url;
+    } else {
+      console.error('Payment error:', data.error);
+    }
+  }
+
+  async function payWithQRIS() {
+    const response = await fetch('http://localhost:5000/api/qris-payment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount: 50000,
+        externalID: 'invoice-12345'
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // console.log(data);
+      localStorage.setItem('id_payment', data.id);
+      localStorage.setItem('id_payment_qr', data.id_qr);
+      setQR(data.qr);
+
+    } else {
+      console.error('Payment error:', data.error);
+    }
+  }
+
+  async function payQRIS() {
+    const response = await fetch('http://localhost:5000/api/qris-payment-submit', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        externalID: localStorage.getItem('id_payment_qr')
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // console.log(data);
+      // localStorage.setItem('id_payment', data.id);
+      
+      // setQR(data.qr);
+
+    } else {
+      console.error('Payment error:', data.error);
+    }
+  }
+
+
   return (
-    <main className="min-h-screen bg-gray-100 p-4">
-        <h1 className="text-4xl font-bold mb-4">Xendit Transaction</h1>
-        <div className="bg-white p-4 rounded-md shadow-md">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2" onClick={checkPay}>Check Balance</button>
-            {amount && <p className="mt-2">Rp {amount}</p>}
-            <label className="block mt-2">
-                <span className="text-gray-700">Harga</span>
-                {/* <input type="number" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" value={price} onChange={e => setPrice(parseInt(e.target.value, 10))} /> */}
-            </label>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2" onClick={payWithOVO}>Pay OVO</button>
+    <main className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-6 flex items-center justify-center">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h1 className="text-3xl font-extrabold text-center text-gray-900 mb-6">Xendit Transaction</h1>
+            <button className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={checkPay}>
+                Check Balance
+            </button>
+            {amount && <p className="mt-4 text-center text-2xl font-large text-gray-800">Saldo Anda : Rp {amount}</p>}
+            <hr className="my-5"/>
+            <div className="mt-4">
+                <label className="block text-2xl font-large text-gray-700 mb-1">Harga : Rp 500.000</label>
+                {/* <input type="number" className="block w-full text-gray-900 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" value={price} onChange={e => setPrice(parseInt(e.target.value, 10))} /> */}
+            </div>
+            <button className="w-full mt-4 bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={payWithOVO}>
+                Pay With OVO
+            </button>
+            <button className="w-full mt-4 bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={payWithDana}>
+                Pay With Dana
+            </button>
+            <button className="w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={payWithShopeePay}>
+                Pay With ShopeePay
+            </button>
+            <button className="w-full mt-4 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={payWithQRIS}>
+                Pay With QRIS
+            </button>
+            {qr && (
+              <div className="mt-6 flex justify-center items-center flex-col">
+                <p className="text-gray-800 font-semibold mb-2">Scan QRIS di bawah ini:</p>
+                <QRCodeSVG value={qr} size={256} />
+                <button className="w-full mt-4 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={payQRIS}>
+                    Pay QRIS
+                </button>
+                 <button className="w-full mt-4 bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300" onClick={checkPayment}>
+                    Check QRIS
+                </button>
+              </div>
+            )}
         </div>
     </main>
   );
--- server/api.js
+++ server/api.js
@@ -4,12 +4,12 @@ import cors from 'cors';
 const secretKey = 'xnd_development_tXbcsCoAUjYAqB0YBhSverOSKY52hAASzOG06ggAz2LzXnouBM4UeFU83KD0U2LR';
 const allowedOrigins = ['http://localhost:3000'];
 
-import { Xendit, Balance as BalanceClient, PaymentRequest as PaymentRequestClient} from 'xendit-node';
+import { Balance as BalanceClient, PaymentRequest as PaymentRequestClient} from 'xendit-node';
 const xenditBalanceClient = new BalanceClient({secretKey: secretKey});
 const xenditPaymentRequestClient = new PaymentRequestClient({secretKey: secretKey});
 
 const app = express();
-const PORT = 5050;
+const PORT = 5000;
 
 app.use(express.json()); 
 
@@ -44,7 +44,67 @@ app.post('/api/ovo-payment', async (req, res) => {
       paymentMethod: {
         ewallet: {
           channelProperties: {
-            mobile_number: '+628123456789',
+            mobileNumber: '6281234567890'
+          },
+          channelCode: 'OVO',
+        },
+        reusability: 'ONE_TIME_USE',
+        type: 'EWALLET',
+      },
+      currency: 'IDR',
+      referenceId: 'example-ref-1234',
+    };
+
+    const response = await xenditPaymentRequestClient.createPaymentRequest({
+      data,
+    });
+
+    res.status(200).json({ 
+      id: response.id
+    });
+  
+});
+
+app.post('/api/dana-payment', async (req, res) => {
+
+    const data = {
+      country: 'ID',
+      amount: 15000,
+      paymentMethod: {
+        ewallet: {
+          channelProperties: {
+            mobileNumber: '6281234567890',
+            successReturnUrl: 'http://localhost:3000/admin',
+          },
+          channelCode: 'DANA',
+        },
+        reusability: 'ONE_TIME_USE',
+        type: 'EWALLET',
+      },
+      currency: 'IDR',
+      referenceId: 'example-ref-1234',
+    };
+
+    const response = await xenditPaymentRequestClient.createPaymentRequest({
+      data,
+    });
+
+    res.status(200).json({ 
+      id: response.id, 
+      url: response.actions[0].url
+    });
+  
+});
+
+app.post('/api/shopeepay-payment', async (req, res) => {
+
+    const data = {
+      country: 'ID',
+      amount: 15000,
+      paymentMethod: {
+        ewallet: {
+          channelProperties: {
+            mobileNumber: '6281234567890',
             successReturnUrl: 'http://localhost:3000/admin',
           },
           channelCode: 'SHOPEEPAY',
@@ -64,6 +124,51 @@ app.post('/api/ovo-payment', async (req, res) => {
       id: response.id, 
       url: response.actions[0].url
     });
+  
+});
+
+app.post('/api/qris-payment', async (req, res) => {
+
+  const data = {
+    amount: 15000,
+    metadata: {
+      sku: 'example-sku-1234'
+    },
+    paymentMethod: {
+      qrCode: {
+        channelCode: 'QRIS'
+      },
+      reusability: 'ONE_TIME_USE',
+      type: 'QR_CODE'
+    },
+    currency: 'IDR',
+    referenceId: 'example-ref-1234'
+  };
+
+    const response = await xenditPaymentRequestClient.createPaymentRequest({data});
+
+    res.status(200).json({ 
+      id: response.id, 
+      id_qr: response.paymentMethod.id, 
+      qr: response.paymentMethod.qrCode.channelProperties.qrString
+    });
+  
+});
+
+app.post('/api/qris-payment-submit', async (req, res) => {
+
+  const response = await fetch(`https://api.xendit.co/qr_codes/${req.body.externalID}/payments/simulate`, {
+    method: 'POST',
+    headers: {
+      Authorization:
+        'Basic ' + Buffer.from(secretKey + ':').toString('base64'),
+    },
+  });
+
+  console.log('ddd',`https://api.xendit.co/qr_codes/${req.body.externalID}/payments/simulate`);
+
+  const data = await response.json();
+  res.status(200).json(data);
   
 });
 
